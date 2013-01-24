835992d13714da2136f2ec6f711cf9e2cc60aa18
honnix <hxliang1982@gmail.com>
2013-01-24 22:01:29 +0100

    button 3 to play shufflely

--- douban_fm/douban_fm.ino
+++ douban_fm/douban_fm.ino
@@ -2,7 +2,7 @@ int LENGTH = 3;
 
 int ledPins[] = {12, 11, 10};
 int buttonPins[] = {5, 4, 3};
-int channelIds[] = {0, 32, 8};
+int channelIds[] = {0, 32, -1};
 
 void setup() {
   Serial.begin(9600);
@@ -15,14 +15,14 @@ void setup() {
 void loop() {
   for (int i = 0; i < LENGTH; ++i) {
     if (digitalRead(buttonPins[i])) {
-      turnOnLed(i);
+      switchChannel(i);
     }
   }
 
   delay(1);
 }
 
-void turnOnLed(int which) {
+void switchChannel(int which) {
   for (int i = 0; i < LENGTH; ++i) {
     if (i == which) {
       digitalWrite(ledPins[i], HIGH);
--- lib/douban.fm.arduino/version.rb
+++ lib/douban.fm.arduino/version.rb
@@ -1,3 +1,3 @@
 module DoubanFMArduino
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
 end
